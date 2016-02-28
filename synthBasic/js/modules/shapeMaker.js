98c388d631808891df6c3996454e04f2bc34c230
william-bratches <williambratches@gmail.com>
2016-02-27 19:44:45 -0500

    modularized cube creation

--- synthBasic/js/modules/shapeMaker.js
+++ synthBasic/js/modules/shapeMaker.js
@@ -1,5 +1,18 @@
 Modules.ShapeMaker = (function() {
+  function createCube(scale) {
+    var scaler = scale || 50;
+    var materialCreator = new THREE.MTLLoader.MaterialCreator();
+  	materialCreator.crossOrigin = 'anonymous';
+  	var url = 'models/cube/altspace-logo.jpg';
+  	//hosted: 'https://sdk.altvr.com/examples/models/share/cube/altspace-logo.jpg';
+  	var texture = materialCreator.loadTexture(url);
+  	var geometry = new THREE.BoxGeometry(1, 1, 1);
+  	var material = new THREE.MeshBasicMaterial({color:'#ffffff', map: texture});
+  	var cube = new THREE.Mesh(geometry, material);
+    cube.scale.multiplyScalar(scaler);
+    return cube;
+  }
   return {
-    
+    createCube: createCube,
   }
 })();

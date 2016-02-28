245d3da74eeb482111cda928a711b839209a02c0
william-bratches <williambratches@gmail.com>
2016-02-27 19:52:37 -0500

    sin and saw cubes

--- synthBasic/js/modules/shapeMaker.js
+++ synthBasic/js/modules/shapeMaker.js
@@ -1,14 +1,12 @@
 Modules.ShapeMaker = (function() {
-  function createCube(scale) {
+  function createCube(scale, color) {
     var scaler = scale || 50;
+    var color = color || '#ffffff'
     var materialCreator = new THREE.MTLLoader.MaterialCreator();
-  	materialCreator.crossOrigin = 'anonymous';
-  	var url = 'models/cube/altspace-logo.jpg';
-  	//hosted: 'https://sdk.altvr.com/examples/models/share/cube/altspace-logo.jpg';
-  	var texture = materialCreator.loadTexture(url);
   	var geometry = new THREE.BoxGeometry(1, 1, 1);
-  	var material = new THREE.MeshBasicMaterial({color:'#ffffff', map: texture});
+  	var material = new THREE.MeshBasicMaterial({color: color});
   	var cube = new THREE.Mesh(geometry, material);
+    materialCreator.crossOrigin = 'anonymous';
     cube.scale.multiplyScalar(scaler);
     return cube;
   }
